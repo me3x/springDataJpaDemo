1d5b68bdadf4927885d3b4bf1fbc5b97eef912ab
Matus.Hennel <matus.hennel@4finance.com>
2018-09-21 04:34:06 +0200

    REST API test

---
+++ src/test/java/me3x/PartRepoTest.groovy
@@ -0,0 +1,34 @@
+package me3x
+
+import groovy.json.JsonSlurper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import spock.lang.Specification
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class PartRepoTest extends Specification {
+
+    @Autowired
+    TestRestTemplate restTemplate
+
+    @Value('${spring.data.rest.basePath}')
+    String restBasePath
+
+    void setup() {
+    }
+
+    void cleanup() {
+    }
+
+    def "Should return parts"() {
+        when:
+            def response = restTemplate.getForObject("${restBasePath}/parts", String.class)
+        then:
+            def json = new JsonSlurper().parseText(response)
+            json._embedded.parts[0].name == 'Matt'
+    }
+}
